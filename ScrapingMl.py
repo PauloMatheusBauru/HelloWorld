92dbddbc83a780646c9dee525525577b78475da2
PauloMatheusBauru <95717547+PauloMatheusBauru@users.noreply.github.com>
2022-03-02 09:10:26 -0300

    Update ScrapingMl.py

--- ScrapingMl.py
+++ ScrapingMl.py
@@ -3,12 +3,6 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import os
-import urllib
-import requests
-import time
-import re
 
 x = 0
 y = 1
